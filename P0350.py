3aa7ff5e223e68569dda16801383ad66a2995fe2
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:50:17 -0800

    Update P0350.py

--- P0350.py
+++ P0350.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        result_set=set(nums1) & set(nums2)
+        result=list(result_set)
+        for element in result_set:
+            count=min(nums1.count(element),nums2.count(element))
+            for i in range(0,count-1):
+                result.append(element)
+        return result

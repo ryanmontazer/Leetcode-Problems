067c11b72bead69bc82305a1322226a9be807976
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:20:28 -0800

    Update P0088.py

--- P0088.py
+++ P0088.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        i,j,cursor=0,0,0
+        while j<n and cursor<n+m:
+            if nums2[j]<nums1[cursor] or i>=m:
+                nums1.insert(cursor,nums2[j])
+                nums1.pop()
+                cursor+=1
+                j+=1
+            else:
+                i+=1
+                cursor+=1

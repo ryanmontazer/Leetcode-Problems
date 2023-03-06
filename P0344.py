a97ef515abea30f1e74d0777abf8e4bf087adf73
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:48:06 -0800

    Update P0344.py

--- P0344.py
+++ P0344.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        length=len(s)
+        for i in range (0,length//2):
+            s[i],s[length-i-1]=s[length-i-1],s[i] 

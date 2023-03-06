53c5c0944ce322e2d2375be5917eac674432bebc
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:39:39 -0800

    Update P0206.py

--- P0206.py
+++ P0206.py
@@ -1,1 +1,19 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        prev=head
+        if head is None:
+            return head
+        curr=head.next
+        head.next=None
+        while curr is not None:
+            new_node=curr.next
+            curr.next=prev
+            prev=curr
+            curr=new_node
+        head=prev
+        return head

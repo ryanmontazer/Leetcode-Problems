6b87adb9f05acd2a4b582cebde041346be534929
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:29:28 -0800

    Update P0150.py

--- P0150.py
+++ P0150.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        length,stack=len(tokens),[]
+        for i in range(0,length):
+            if tokens[i] not in {"+","-","*","/"}:
+                stack.append(int(tokens[i]))
+            elif tokens[i]=="+":
+                stack.append(stack.pop()+stack.pop())
+            elif tokens[i]=="-":
+                stack.append(0-stack.pop()+stack.pop())
+            elif tokens[i]=="*":
+                stack.append(stack.pop()*stack.pop())
+            elif tokens[i]=="/":
+                stack.append(int(1/stack.pop()*stack.pop()))
+        return stack.pop()

66e32b0a1eb5b84979734a877582ab2099e76c72
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:34:38 -0800

    Update P0162.py

--- P0162.py
+++ P0162.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        length=len(nums)
+        left,right=0,length-1
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]>nums[mid+1]:
+                right=mid
+            else:
+                left=mid+1
+        return left

dccb916eaf03e8ea0a629618fd5d20308bdc15cb
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:49:42 -0800

    Update P0349.py

--- P0349.py
+++ P0349.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        length1,length2=len(nums1),len(nums2)
+        result=[]
+        if min (length1,length2)==0:
+            return result
+        for i in range(0,length1):
+            for j in range(0,length2):
+                if nums1[i]==nums2[j]:
+                    result.append(nums1[i])
+                    break
+        result=list(set(result))
+        return result

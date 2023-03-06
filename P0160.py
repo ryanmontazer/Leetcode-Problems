1c78b950d5a54741eb48529d91922240126f6fae
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:33:52 -0800

    Update P0160.py

--- P0160.py
+++ P0160.py
@@ -1,1 +1,17 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        nodeA_set=set()
+        while headA is not None:
+            nodeA_set.add(headA)
+            headA=headA.next
+        while headB is not None:
+            if headB in nodeA_set:
+                return headB
+            headB=headB.next
+        return None

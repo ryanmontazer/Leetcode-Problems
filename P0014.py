958b1050be302340fa0ad2e1ba46d08c8ad0926f
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:06:27 -0800

    Update P0014.py

--- P0014.py
+++ P0014.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        length=len(strs)
+        minimum=len(strs[0])
+        for i in range(1,length):
+            minimum=min(minimum,len(strs[i]))
+        result=""
+        for i in range(0,minimum):
+            for j in range(1,length):
+                if strs[j][i] != strs[0][i]:
+                    return result
+            result=result+strs[0][i]
+        return result

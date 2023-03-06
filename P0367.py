1bd650e055c48a2e9f40d8386dc1346ec7f31e7e
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:51:08 -0800

    Update P0367.py

--- P0367.py
+++ P0367.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def isPerfectSquare(self, num: int) -> bool:
+        left,right=1,num//2+1
+        #cover 1-4 later
+        while left+1<right:
+            mid=(left+right)//2
+            if mid**2>num:
+                right=mid
+            else:
+                left=mid
+        if left**2==num:
+            return True
+        return False

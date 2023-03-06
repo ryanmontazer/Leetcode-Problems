9457d25e3c1df161a9d7ce1ae38dba1d448e7fe2
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:15:07 -0800

    Update P0050.py

--- P0050.py
+++ P0050.py
@@ -1,1 +1,20 @@
-
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        if n<0:
+            n,x=-n,1/x
+        result=1
+        memory=[]
+        while n>0:
+            if n%2==0:
+                n=n/2
+                memory.append('square')
+            else:
+                n-=1
+                memory.append('plus')
+        while len(memory)>0:
+            if memory[-1]=='plus':
+                result*=x
+            else:
+                result=result**2
+            del memory[-1]
+        return result

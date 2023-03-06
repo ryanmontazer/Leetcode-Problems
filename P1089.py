9bc9fd869747ff4bf451d3232cbe94a9d6c9d4b5
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 15:16:07 -0800

    Update P1089.py

--- P1089.py
+++ P1089.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def duplicateZeros(self, arr: List[int]) -> None:
+        """
+        Do not return anything, modify arr in-place instead.
+        """
+        length=len(arr)
+        i=0
+        while i<length:
+            if arr[i]==0:
+                for j in range(length-1,i+1,-1):
+                    arr[j]=arr[j-1]
+                if i+1<length:
+                    arr[i+1]=0
+                i+=2
+            else:
+                i+=1

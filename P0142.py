c4d9a8a344237a6d6111e3817133e5eac112fb61
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:27:18 -0800

    Update P0142.py

--- P0142.py
+++ P0142.py
@@ -1,1 +1,15 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        visited_nodes=set()
+        while head is not None:
+            if head in visited_nodes:
+                return head
+            visited_nodes.add(head)
+            head=head.next
+        return None

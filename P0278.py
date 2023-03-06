37db53460626558ad6ce67e680d1a5c42a2e3377
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:42:43 -0800

    Update P0278.py

--- P0278.py
+++ P0278.py
@@ -1,1 +1,20 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
 
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        if n==1:
+            return 1
+        left,right=1,n+1
+        while left<right:
+            mid=(left+right)//2
+            if not isBadVersion(mid) and isBadVersion(mid+1):
+                return (mid+1)
+            elif not isBadVersion(mid):
+                left=mid+1
+            elif isBadVersion(mid) and mid==1:
+                return mid
+            elif isBadVersion(mid):
+                right=mid
+        if left != n and not isBadVersion(left) and isBadVersion(left+1):
+            return left

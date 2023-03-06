2a033fca0ca8dc4a2067f05ff4e123cc45e5983c
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 15:09:32 -0800

    Update P0744.py

--- P0744.py
+++ P0744.py
@@ -1,1 +1,16 @@
-
+class Solution:
+    def nextGreatestLetter(self, letters: List[str], target: str) -> str:
+        length=len(letters)
+        #letters are non-decreasing so letters[i]-target>0 is like FFFTTTTT
+        #find first T or if all F , then first F
+        left,right=0,length-1
+        while left<right:
+            mid=(left+right)//2
+            if letters[mid]>target:
+                right=mid
+            else:
+                left=mid+1
+        if letters[right]>target:
+            return letters[right]
+        else:
+            return letters[0]

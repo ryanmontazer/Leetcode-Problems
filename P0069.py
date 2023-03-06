29ad7580768c3cf2935f9d50b079f676040fd57c
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:19:19 -0800

    Update P0069.py

--- P0069.py
+++ P0069.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        if x<2:
+            return x
+        index=[2,x//2]
+        while index[0]<=index[1]:
+            mid=(index[0]+index[1])//2
+            if mid**2==x:
+                return mid
+            if mid**2>x:
+                index[1]=mid-1
+            else:
+                index[0]=mid+1
+        return index[1]

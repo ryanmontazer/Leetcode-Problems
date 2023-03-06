ca12a3c8efd9b1e12a3db389a3af55f44d7268fa
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:45:59 -0800

    Update P0287.py

--- P0287.py
+++ P0287.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def findDuplicate(self, nums: List[int]) -> int:
+        length=len(nums)
+        for i in range(0,length):
+            if nums[abs(nums[i])]>0:
+                nums[abs(nums[i])]*=-1
+            else:
+                return abs(nums[i])

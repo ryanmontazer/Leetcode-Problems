f7bc58872f394b988801b7511621d3ec148a6180
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 15:08:48 -0800

    Update P0724.py

--- P0724.py
+++ P0724.py
@@ -1,1 +1,12 @@
-
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        total_sum,left_sum=0,0
+        for i in range(0,len(nums)):
+            total_sum+=nums[i]
+        if total_sum-nums[0]==0:
+            return 0
+        for i in range(0,len(nums)-1):
+            left_sum+=nums[i]
+            if left_sum==total_sum-left_sum-nums[i+1]:
+                return i+1
+        return -1

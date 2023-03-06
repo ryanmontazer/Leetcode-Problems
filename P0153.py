aa1bc0e5c2ed627a6569cb20a8918ec5973228b3
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:31:07 -0800

    Update P0153.py

--- P0153.py
+++ P0153.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        length=len(nums)
+        left,right=0,length-1
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]>nums[right]:
+                left=mid+1
+            else:
+                right=mid
+        return nums[left]

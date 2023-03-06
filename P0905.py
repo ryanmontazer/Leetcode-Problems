a93c7212c018d9c800ecc8672cacef386d7a31fc
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 15:12:20 -0800

    Update P0905.py

--- P0905.py
+++ P0905.py
@@ -1,1 +1,19 @@
-
+class Solution:
+    def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        length=len(nums)
+        odds=0
+        for i in range (0,length):
+            if nums[i]%2==1:
+                odds+=1
+        test=0
+        i=0
+        while test!=odds:
+            if nums[i]%2==1:
+                test +=1
+                k=nums[i]
+                for j in range(i,length-1):
+                    nums[j]=nums[j+1]
+                nums[length-1]=k
+            else:
+                i+=1
+        return (nums)

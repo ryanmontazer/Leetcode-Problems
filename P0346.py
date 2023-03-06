5d6b4398a59a2c5d239242fd8f78c174504e584c
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:48:49 -0800

    Update P0346.py

--- P0346.py
+++ P0346.py
@@ -1,1 +1,31 @@
+class MovingAverage:
 
+    def __init__(self, size: int):
+        self.size=size
+        self.count=0
+        self.sum=0
+        self.queue=[0.1]*size
+        self.head=-1
+        self.tail=-1
+
+    def next(self, val: int) -> float:
+        if self.count<self.size: #In this case just add the element at tail
+            self.head=0
+            self.tail=self.count
+            self.queue[self.tail]=val
+            self.count+=1
+            self.sum+=val
+            return self.sum/self.count
+        else:
+            self.sum=self.sum+val-self.queue[self.head]
+            self.head=(self.head+1) % self.size
+            self.tail=(self.tail+1) % self.size
+            self.queue[self.tail]=val
+            return self.sum/self.size
+            
+            
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)

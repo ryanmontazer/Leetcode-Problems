dbaaf21874d60691247fedc24f838172b1e88b54
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:46:55 -0800

    Update P0328.py

--- P0328.py
+++ P0328.py
@@ -1,1 +1,33 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None or head.next.next is None:
+            return head
+        ll_tail=head
+        while ll_tail.next is not None:
+            ll_tail=ll_tail.next
+        ll_tail_new=ll_tail
+        prev=head
+        curr=head.next
+        while True:
+            if prev is ll_tail:
+                return head
+            elif curr is ll_tail:
+                prev.next=ll_tail.next
+                ll_tail_new.next=curr
+                ll_tail_new=curr
+                ll_tail_new.next=None
+                return head
+            else:
+                prev_update=curr.next
+                curr_update=curr.next.next
+                prev.next=curr.next
+                ll_tail_new.next=curr
+                ll_tail_new=curr
+                ll_tail_new.next=None
+                prev=prev_update
+                curr=curr_update

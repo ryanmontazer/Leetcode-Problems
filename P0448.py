fb206ab48e6e45a37b470765780a90227d2afd6a
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:54:25 -0800

    Update P0448.py

--- P0448.py
+++ P0448.py
@@ -1,1 +1,14 @@
-
+class Solution:
+    def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        n= len(nums)
+        repeated= [-1]*n
+        for num in nums:
+            repeated[num-1]=-2
+        for i in range(0,n):
+            if repeated[i]==-1:
+                repeated[i]=i+1
+        result=[]
+        for i in range(0,n):
+            if repeated[i]!=-2:
+                result.append(repeated[i])
+        return result

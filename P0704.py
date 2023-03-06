8885d7ff6a2c313e6a2766247ce208f3d00c13d9
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 15:06:38 -0800

    Update P0704.py

--- P0704.py
+++ P0704.py
@@ -1,1 +1,13 @@
-
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        length=len(nums)
+        index=[0,length-1]
+        while index[0]<=index[1]:
+            mid=(index[0]+index[1])//2
+            if nums[mid]==target:
+                return mid
+            if target<nums[mid]:
+                index[1]=mid-1
+            else:
+                index[0]=mid+1
+        return -1

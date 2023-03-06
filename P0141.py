983538b2dd72bb0b32e61f3a1b23de226dc302ea
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:26:37 -0800

    Update P0141.py

--- P0141.py
+++ P0141.py
@@ -1,1 +1,16 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        ll_set=set()
+        while head != None:
+            if head in ll_set:
+                return True
+            else:
+                ll_set.add(head)
+                head=head.next
+        return False

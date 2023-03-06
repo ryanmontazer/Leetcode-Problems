17fc4097d72ea12479055cc973c30e37e4c06363
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 15:02:21 -0800

    Update P0561.py

--- P0561.py
+++ P0561.py
@@ -1,1 +1,8 @@
-
+class Solution:
+    def arrayPairSum(self, nums: List[int]) -> int:
+        length=len(nums)
+        nums.sort()
+        nums_sum=0
+        for i in range(0,length//2):
+            nums_sum += nums[2*i]
+        return nums_sum

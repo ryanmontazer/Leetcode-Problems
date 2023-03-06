7e953f343b1f3d1d867b378bd28337f17509065a
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:10:22 -0800

    Update P0026.py

--- P0026.py
+++ P0026.py
@@ -1,1 +1,9 @@
-
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        length=len(nums)
+        i=1
+        for j in range(1,length):
+            if nums[j] != nums[j-1]:
+                nums[i]=nums[j]
+                i+=1
+        return i

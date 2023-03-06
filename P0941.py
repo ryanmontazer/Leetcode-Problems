682a8f4f7aa0f72e279be2799315b118b3a0e476
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 15:13:20 -0800

    Update P0941.py

--- P0941.py
+++ P0941.py
@@ -1,1 +1,10 @@
-
+class Solution:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        i=0
+        while  i<len(arr)-1 and arr[i+1]-arr[i]>0:
+            i=i+1
+        if i==0 or i==len(arr)-1:
+            return False
+        while i<len(arr)-1 and arr[i+1]-arr[i]<0:
+            i=i+1
+        return i==len(arr)-1

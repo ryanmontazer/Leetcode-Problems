5722c0b68117932ead2cd963bec6c3e57ca172e8
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:41:50 -0800

    Update P0234.py

--- P0234.py
+++ P0234.py
@@ -1,1 +1,17 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        ll_string=[]
+        curr=head
+        while curr is not None:
+            ll_string.append(curr.val)
+            curr=curr.next
+        n=len(ll_string)
+        for i in range(0,n//2):
+            if ll_string[i] != ll_string[n-1-i]:
+                return False
+        return True

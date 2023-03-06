311879937930242e2f58a2ae94ebe9699b760f45
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:11:43 -0800

    Update P0027.py

--- P0027.py
+++ P0027.py
@@ -1,1 +1,17 @@
-
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        length=len(nums)
+        k=0
+        i=0
+        while i<length:
+            if nums[i]!=val:
+                i=i+1
+            elif i<length-1:
+                k+=1
+                for j in range(i,length-1):
+                    nums[j]=nums[j+1]
+                nums[length-1]=0.1
+            elif i==length-1:
+                nums[length-1]=0.1
+                k+=1
+        return length-k

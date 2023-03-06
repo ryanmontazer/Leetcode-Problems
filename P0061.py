e98abf8ebbfc47fd0e20cc1531299d27f2a26017
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-03-06 14:16:48 -0800

    Update P0061.py

--- P0061.py
+++ P0061.py
@@ -1,1 +1,29 @@
-
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+        tail=head
+        length=1
+        while True:
+            if tail.next:
+                tail=tail.next
+                length+=1
+            else:
+                break
+        k=(length-k) % length
+        new_head=head
+        new_tail=tail
+        for i in range(0,k):
+            if i==k-1:
+                new_tail=new_head
+            new_head=new_head.next
+        
+        tail.next=head
+        head=new_head
+        new_tail.next=None
+        return head

dce302b1ab24b73e63a782d73082e21ba4baebe0
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-16 18:07:03 -0700

    Update P0002.py

--- P0002.py
+++ P0002.py
@@ -1,3 +1,32 @@
+# Solution 1
+# Time: O(n)
+# Space: O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy_node=ListNode(0)
+        current=dummy_node
+        carry=0
+        while l1 or l2:
+            val=carry
+            if l1:
+                val += l1.val
+                l1= l1.next
+            if l2:
+                val+=l2.val
+                l2=l2.next
+            carry,val= divmod(val,10)
+            current.next=ListNode(val)
+            current=current.next
+        if carry:
+            current.next=ListNode(carry)
+        return dummy_node.next
+
+# Solution 2
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):

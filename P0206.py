20d948c8549d9a197447ab1a70c05e932cf11893
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-17 20:53:05 -0700

    Update P0206.py

--- P0206.py
+++ P0206.py
@@ -1,3 +1,21 @@
+# Solution 1
+# Time : O(n)
+# Space : O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        curr, prev =head, None
+        while curr:
+            new_curr=curr.next
+            curr.next=prev
+            prev=curr
+            curr=new_curr
+        return prev
+# Solution 2
 # Definition for singly-linked list.
 # class ListNode:
 #     def __init__(self, val=0, next=None):

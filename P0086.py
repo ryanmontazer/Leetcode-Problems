c548a24473cc02c60ba08b48e7904e193ac838b7
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-20 20:02:45 -0700

    Update P0086.py

--- P0086.py
+++ P0086.py
@@ -1,3 +1,29 @@
+# Solution 1
+# Time O(n)
+# Space O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        curr, par1_head, par2_head= head, ListNode(0), ListNode(0)
+        curr_par1, curr_par2= par1_head, par2_head
+        while curr:
+            #Assigning curr to either par1 or par2
+            if curr.val<x:
+                curr_par1.next=curr
+                curr_par1=curr_par1.next
+            else:
+                curr_par2.next=curr
+                curr_par2=curr_par2.next
+            curr=curr.next
+        curr_par1.next=par2_head.next
+        curr_par2.next=None
+        return par1_head.next
+
+
 # Solution 2
 # Time O(n)
 # Space O(1)

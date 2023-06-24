f3ce9e40730e59594622658c705f8feb0f8b8ee0
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-23 17:29:52 -0700

    Update P0003.py

--- P0003.py
+++ P0003.py
@@ -1,1 +1,11 @@
-
+# Time: O(n)
+# Space: O(min(m,n))
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        char_map, max_length, start_curr= {}, 0, 0
+        for curr in range(len(s)):
+            if s[curr] in char_map:
+                start_curr=max(start_curr,char_map[s[curr]]+1)
+            char_map[s[curr]]=curr
+            max_length=max(max_length,curr-start_curr+1)
+        return max_length

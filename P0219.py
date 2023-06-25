993f093521182191b54074b82d9aff020d0a22f9
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-24 18:52:51 -0700

    Update P0219.py

--- P0219.py
+++ P0219.py
@@ -1,1 +1,14 @@
-
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        my_dict={}
+        for index in range(len(nums)):
+            if nums[index] in my_dict:
+                for i in my_dict[nums[index]]:
+                    if abs(index-i)<=k:
+                        return True
+                my_dict[nums[index]].append(index)
+            else:
+                my_dict[nums[index]]=[index]
+        return False

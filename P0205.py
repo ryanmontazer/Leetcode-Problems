d3cda4e0a26548dc772af68763141bba07792450
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-24 20:46:18 -0700

    Update P0205.py

--- P0205.py
+++ P0205.py
@@ -1,1 +1,15 @@
-
+# Solution 1
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        dict_s, dict_t= {}, {}
+        for i in range(len(s)):
+            if not s[i] in dict_s:
+                dict_s[s[i]]=i
+            if not t[i] in dict_t:
+                dict_t[t[i]]=i
+        for i in range(len(s)):
+            if not dict_s[s[i]]==dict_t[t[i]]:
+                return False
+        return True

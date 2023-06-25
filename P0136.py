50a970faed7a5520a769f0ca0392029dce8b9420
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-25 00:04:53 -0700

    Update P0136.py

--- P0136.py
+++ P0136.py
@@ -1,1 +1,14 @@
-
+# Solution 2
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        my_dict={}
+        for num in nums:
+            if num in my_dict:
+                my_dict[num]+=1
+            else:
+                my_dict[num]=1
+        for key1 in my_dict:
+            if my_dict[key1]==1:
+                return key1

3adc257643e10cb05cfb41e420178b0f15837a8a
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-25 02:50:35 -0700

    Update P0700.py

--- P0700.py
+++ P0700.py
@@ -1,3 +1,25 @@
+# Solution 1
+# Time: O(h)
+# Space: O(1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        curr=root
+        while curr:
+            if not curr.val-val:
+                return curr
+            elif curr.val>val:
+                curr=curr.left
+            elif curr.val<val:
+                curr=curr.right
+        return curr
+
+# Solution 2
 # Time: O(n)
 # Space: O(n)
 # Definition for singly-linked list.

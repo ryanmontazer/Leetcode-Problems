4ae11746a212b21522f10df66ccf740ecc9e5439
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-24 23:46:40 -0700

    Update P0202.py

--- P0202.py
+++ P0202.py
@@ -1,1 +1,16 @@
-
+# Time: O(log n)
+# Space: O(log n)
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        my_dict={}
+        while  n!=1 and not n in my_dict:
+            new_number=self.evaluate_squares(n)
+            my_dict[n]=new_number
+            n=new_number
+        return n==1
+    def evaluate_squares(self,n)-> int:
+        result=0
+        while n:
+            result+=(n % 10)**2
+            n=(n- n % 10)/10
+        return result

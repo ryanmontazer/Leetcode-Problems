dae61ac65dd8de213c2b7214746d591555d10ad7
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-25 00:06:31 -0700

    Update P0136.py

--- P0136.py
+++ P0136.py
@@ -1,3 +1,13 @@
+# Solution 1
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        result=0
+        for num in nums:
+            result ^= num
+        return result
+
 # Solution 2
 # Time: O(n)
 # Space: O(n)

721e3a8a6c0395e41531644cf070b8fa85ba0ed0
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-24 19:02:00 -0700

    Update P0387.py

--- P0387.py
+++ P0387.py
@@ -1,1 +1,14 @@
-
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        my_dict={}
+        for i in range(len(s)):
+            if not s[i] in my_dict:
+                my_dict[s[i]]=0
+            else:
+                my_dict[s[i]]=1
+        for i in range(len(s)):
+            if my_dict[s[i]]==0:
+                return i
+        return -1

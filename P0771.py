0125118e6e500c160f03f913e35ff6e346c60677
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-24 18:25:29 -0700

    Update P0771.py

--- P0771.py
+++ P0771.py
@@ -1,1 +1,11 @@
-
+# Time: O(m+n)
+# Space: O(n)
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        my_set, count=set(), 0
+        for char in jewels:
+            my_set.add(char)
+        for char in stones:
+            if char in my_set:
+                count+=1
+        return count

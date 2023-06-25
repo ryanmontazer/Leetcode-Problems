8f09cd94b580e31e01c5a78f46aca7517d0124e8
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-24 21:05:25 -0700

    Update P0001.py

--- P0001.py
+++ P0001.py
@@ -1,1 +1,10 @@
-
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        my_dict={}
+        for i in range(len(nums)):
+            if target-nums[i] in my_dict:
+                return [i,my_dict[target-nums[i]]]
+            else:
+                my_dict[nums[i]]=i

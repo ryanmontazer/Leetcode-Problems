bf9c89ba72aa3fe7d07d4f2c24d9b7f2d984a505
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-06-26 21:16:53 -0700

    Update P0841.py

--- P0841.py
+++ P0841.py
@@ -2,17 +2,15 @@
 # Space: O(n)
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        my_set, visited,  queue=set(), set() ,[[rooms[0],0]]
+        visited,  queue=set (),[[rooms[0],0]]
         visited.add(0)
         while queue:
             element= queue.pop(0)
             keys=element[0]
             visited.add(element[1])
             for key in keys:
-                if key != 0 :
-                    my_set.add(key)
                 if not key in visited :
                     queue.append([rooms[key],key])
-        if len(my_set)==len(rooms)-1:
+        if len(visited)==len(rooms):
             return True
         return False

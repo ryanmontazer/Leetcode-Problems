cbfe0a11168c4a28759a425b271dfb1e8e41fbdd
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-06 21:18:00 -0700

    Update P0144.py

--- P0144.py
+++ P0144.py
@@ -1,3 +1,5 @@
+# Time: O(n)
+# Space: O(h)
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,17 +8,17 @@
 #         self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        stack=[]
-        result=[]
-        current=root
+        stack,result,curr=[],[],root
         while True:
-            if current:
-                result.append(current.val)
-                stack.append(current)
-                current=current.left
+            #Traversing to the leftmost node of the curr node
+            if curr:
+                #Add the node to the stack & result before going to its left child
+                result.append(curr.val)
+                stack.append(curr)
+                curr=curr.left
             elif stack:
-                current=stack.pop()
-                current=current.right
+                #Backtrack from the empty subtree and visit the node at the top of the stack, if stack is empty we are done!
+                curr=stack.pop()
+                curr=curr.right
             else:
-                break
-        return result
+                return result

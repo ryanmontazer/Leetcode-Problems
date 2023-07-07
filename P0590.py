a2f77ccb8dc4ed24855718e9a10f512ca972c5b2
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-06 19:03:57 -0700

    Update P0590.py

--- P0590.py
+++ P0590.py
@@ -1,1 +1,22 @@
+# Recursive Solution
+# Time: O(n)
+# Space: O(h)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
 
+class Solution:
+    def postorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        if not root.children:
+            return [root.val]
+        result=[]
+        for child in root.children:
+            result=result+self.postorder(child)
+        result=result+[root.val]
+        return result

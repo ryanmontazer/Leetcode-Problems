e49c03a159470f37fffbf4ec31f9ee1cdccd8a8e
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 01:19:28 -0700

    Update P0589.py

--- P0589.py
+++ P0589.py
@@ -1,1 +1,22 @@
+# Solution 2: Recursion
+# Time: O(n)
+# Space: O(h)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
 
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        result=[]
+        if not root:
+            return result
+        if not root.children:
+            return [root.val]
+        result.append(root.val)
+        for child in root.children:
+            result=result+self.preorder(child)
+        return result

963558da04a902746f8b54b1c434bb75d93562d7
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 02:24:29 -0700

    Update P0144.py

--- P0144.py
+++ P0144.py
@@ -1,3 +1,27 @@
+# Solution 1: Using Stack
+# Time: O(n)
+# Space: O(h)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        stack,result= [root],[]
+        while stack:
+            node = stack.pop()            
+            result.append(node.val)            
+            if node.right:
+                stack.append(node.right)
+            if node.left:
+                stack.append(node.left)
+        return result
+
+# Solution 2: Using Stack
 # Time: O(n)
 # Space: O(h)
 # Definition for a binary tree node.

1a2233e4e7e51186ef9e21a72271c964a8d7047e
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 02:43:48 -0700

    Update P0094.py

--- P0094.py
+++ P0094.py
@@ -23,26 +23,3 @@ class Solution:
                 curr=curr.right
             else:
                 return result
-
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
-        result=[]
-        stack=[]
-        current = root
-        while True:
-            if current:
-                stack.append(current)
-                current=current.left
-            elif stack:
-                current=stack.pop()
-                result.append(current.val)
-                current=current.right
-            else:
-                break
-        return result

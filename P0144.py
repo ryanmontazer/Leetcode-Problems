27a9aac6395ec69d7e7026c6d72ec2e730f93085
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 03:00:16 -0700

    Update P0144.py

--- P0144.py
+++ P0144.py
@@ -46,3 +46,20 @@ class Solution:
                 curr=curr.right
             else:
                 return result
+
+# Solution 3: Recursion
+# Time: O(n)
+# Space: O(h)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        if not root.left and not root.right:
+            return [root.val]
+        return [root.val]+self.preorderTraversal(root.left)+self.preorderTraversal(root.right)

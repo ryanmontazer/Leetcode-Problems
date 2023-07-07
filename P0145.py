4a080398d8adcbf7eeaba4b82be6b8967be909a4
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-06 21:30:39 -0700

    Update P0145.py

--- P0145.py
+++ P0145.py
@@ -1,4 +1,31 @@
-# Solution 1: Recursion
+# Solution 1: using stack
+# Time: O(n)
+# Space: O(h)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result=[]
+        if not root :
+            return result
+        stack=[[root,False]]
+        while stack:
+            node,visited= stack.pop()
+            if visited:
+                result.append(node.val)
+            else:
+                stack.append([node,True])
+                if node.right:
+                    stack.append([node.right,False])
+                if node.left:
+                    stack.append([node.left,False])
+        return result
+
+# Solution 2: Recursion
 # Time: O(n)
 # Space: O(h)
 # Definition for a binary tree node.

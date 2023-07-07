e64449f465ff72848efad56dbe5ee4093d96cd66
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 14:27:18 -0700

    Update P0739.py

--- P0739.py
+++ P0739.py
@@ -1,1 +1,21 @@
-
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result=[1]*len(temperatures)
+        stack=[0]
+        for i in range(1,len(temperatures)):
+            if stack and temperatures[i]<=temperatures[stack[-1]]:
+                stack.append(i)
+            else:
+                while stack :
+                    index=stack[-1]
+                    if temperatures[index]<temperatures[i]:
+                        result[index]=i-index
+                        stack.pop()
+                    else:
+                        break
+                stack.append(i)
+        while stack:
+            result[stack.pop()]=0
+        return result

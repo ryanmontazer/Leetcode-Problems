756baeebf5039931c880d9486a14b1b3e2403691
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 02:29:57 -0700

    Update P0589.py

--- P0589.py
+++ P0589.py
@@ -1,3 +1,26 @@
+# Solution 1: Using Stack
+# Time: O(n)
+# Space: O(h)
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        stack,result=[root],[]
+        while stack:
+            node=stack.pop()
+            result.append(node.val)
+            for child in reversed(node.children):
+                stack.append(child)
+        return result
+
 # Solution 2: Recursion
 # Time: O(n)
 # Space: O(h)

ac1fd69f3f98864e4d489a4a33aa15ddb368041b
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 19:53:03 -0700

    Update P0235.py

--- P0235.py
+++ P0235.py
@@ -1,1 +1,21 @@
+# Time: O(h)
+# Space: O(1)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if (root.val-p.val)*(root.val-q.val)<0:
+            return root
+        elif root.val==p.val:
+            return p
+        elif root.val==q.val:
+            return q
+        elif root.val<p.val:
+           return self.lowestCommonAncestor(root.right,p,q)
+        else:
+            return self.lowestCommonAncestor(root.left,p,q)

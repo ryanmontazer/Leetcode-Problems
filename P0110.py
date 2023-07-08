177766d1d13129c7684c5cb056916df0b14ab66d
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 19:27:20 -0700

    Update P0110.py

--- P0110.py
+++ P0110.py
@@ -1,1 +1,21 @@
-
+# Time: O(n)
+# Space: O(h)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        if not root:
+            return True       
+        if abs(self.depthBT(root.left)-self.depthBT(root.right))<=1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+            return True
+        return False
+    def depthBT(self,root) -> int:
+        if not root:
+            return 0
+        if not root.left and not root.right:
+            return 1
+        return 1+max(self.depthBT(root.left),self.depthBT(root.right))

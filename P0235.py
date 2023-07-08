984e06e6911d6fe48aab353c9ceb6bfc511e3535
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 19:59:41 -0700

    Update P0235.py

--- P0235.py
+++ P0235.py
@@ -11,10 +11,8 @@ class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         if (root.val-p.val)*(root.val-q.val)<0:
             return root
-        elif root.val==p.val:
-            return p
-        elif root.val==q.val:
-            return q
+        elif (root.val-p.val)*(root.val-q.val)==0:
+            return root
         elif root.val<p.val:
            return self.lowestCommonAncestor(root.right,p,q)
         else:

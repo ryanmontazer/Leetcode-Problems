68221b4ab2d4092850ed5bbfc539f66660f08723
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-07 20:40:06 -0700

    Update P0700.py

--- P0700.py
+++ P0700.py
@@ -18,18 +18,3 @@ class Solution:
             elif curr.val<val:
                 curr=curr.right
         return curr
-
-# Solution 2
-# Time: O(n)
-# Space: O(n)
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-class Solution:
-    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
-            return head
-        new_head, new_head.next, head.next =head.next, head, self.swapPairs(head.next.next)
-        return new_head

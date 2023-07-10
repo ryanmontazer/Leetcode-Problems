c0ecf5eab4241d024d211dd6f11a4c2bc09eaa40
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-10 14:44:18 -0700

    Update P1046.py

--- P1046.py
+++ P1046.py
@@ -5,8 +5,7 @@ class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         for i in range(len(stones)):
             stones[i]*=-1
-        if len(stones)>1:
-            heapq.heapify(stones)
+        heapq.heapify(stones)
         while len(stones)>1:
             first_small=heapq.heappop(stones)
             second_small=heapq.heappop(stones)

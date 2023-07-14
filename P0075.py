af341d2d13832bc6cebfdb588d2c1c0e9fa5f0b5
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-14 16:37:47 -0700

    Update P0075.py

--- P0075.py
+++ P0075.py
@@ -1,1 +1,48 @@
-
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        last_seen={0:-1,1:-1,2:-1}
+        for i in range(len(nums)):
+            if nums[i]==2:
+                last_seen[2]=i
+                continue
+            if nums[i]==1:
+                if last_seen[2]==-1:
+                    last_seen[1]=i
+                    continue
+                if last_seen[2]!=-1 and last_seen[1]!=-1:
+                    nums[last_seen[1]+1],nums[i]=1,2
+                    last_seen[1],last_seen[2]=last_seen[1]+1,i
+                    continue
+                if last_seen[2]!=-1 and last_seen[1]==-1 and last_seen[0]==-1:
+                    nums[0],nums[i]=1,2
+                    last_seen[1],last_seen[2]=0,i
+                    continue
+                if last_seen[2]!=-1 and last_seen[1]==-1 and last_seen[0]!=-1:
+                    nums[last_seen[0]+1],nums[i]=1,2
+                    last_seen[1],last_seen[2]=last_seen[0]+1,i
+                    continue
+            if nums[i]==0:
+                if last_seen[2]==-1 and last_seen[1]==-1:
+                    last_seen[0]=i
+                    continue
+                if last_seen[2]==-1 and last_seen[1]!=-1:
+                    nums[last_seen[0]+1],nums[i]=0,1
+                    last_seen[0],last_seen[1]=last_seen[0]+1,i
+                    continue
+                if last_seen[1]==-1 and last_seen[2]!=-1:
+                    nums[last_seen[0]+1],nums[i]=0,2
+                    last_seen[0],last_seen[2]=last_seen[0]+1,i
+                    continue
+                if last_seen[1]!=-1 and last_seen[2]!=-1 and last_seen[0]==-1:
+                    nums[0],nums[last_seen[1]+1],nums[i]=0,1,2
+                    last_seen[0],last_seen[1],last_seen[2]=0,last_seen[1]+1,i
+                    continue
+                if last_seen[1]!=-1 and last_seen[2]!=-1 and last_seen[0]!=-1:
+                    nums[last_seen[0]+1],nums[last_seen[1]+1],nums[i]=0,1,2
+                    last_seen[0],last_seen[1],last_seen[2]=last_seen[0]+1,last_seen[1]+1,i
+                    continue
+        """
+        Do not return anything, modify nums in-place instead.
+        """

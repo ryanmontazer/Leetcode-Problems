86b139f09d9a933fa6bc63b08e629ca22a1e2bfc
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-07-13 20:39:41 -0700

    Update P0378.py

--- P0378.py
+++ P0378.py
@@ -1,12 +1,14 @@
-# Time: O(k^2*log(k))
+# Time: O(min(n,k)^2 log(k))
 # Space: O(k)
 import heapq
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        max_heap=[]
-        for rows in range(min(len(matrix),k)):
-            for cols in range(min(len(matrix[0]),k)):
+        max_heap,n=[],len(matrix)
+        for rows in range(n):
+            for cols in range(n):
                 heapq.heappush(max_heap,-matrix[rows][cols])
                 if len(max_heap)>k:
                     heapq.heappop(max_heap)
+                    if -max_heap[0]<matrix[rows][cols]:
+                        break
         return -heapq.heappop(max_heap)

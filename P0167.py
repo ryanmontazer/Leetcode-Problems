887abac53ab9e526592ef0b4c5e1a259e8304a9e
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-08-10 18:09:16 -0700

    Update P0167.py

--- P0167.py
+++ P0167.py
@@ -1,15 +1,12 @@
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         length=len(numbers)
-        if length==2:
-            return [1,2]
-        for i in range(0,length-1):
-            left,right=i+1,length-1
-            while left<=right:
-                mid=(left+right)//2
-                if numbers[mid]==target-numbers[i]:
-                    return [i+1,mid+1]
-                elif numbers[mid]>target-numbers[i]:
-                    right=mid-1
-                else:
-                    left=mid+1
+        left,right=0,length-1
+        while left<right:
+            if numbers[left]+numbers[right]==target:
+                return [left+1,right+1]
+            elif numbers[left]+numbers[right]>target:
+                right-=1
+            elif numbers[left]+numbers[right]<target:
+                left+=1
+        return [-1,-1]

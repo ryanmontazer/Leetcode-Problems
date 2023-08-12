34f8a0902800981408094c7cba99871fdd29c379
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2023-08-11 17:08:09 -0700

    Update P0154.py

--- P0154.py
+++ P0154.py
@@ -1,3 +1,28 @@
+# Solution 1
+# Time: O(log(n))~O(n)
+# Space: O(1)
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        left,right=0,len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]>nums[right]:
+                left=mid+1
+            if nums[mid]==nums[right]:
+                check=True
+                for i in range(mid+1,right):
+                    if nums[i]!=nums[mid]:
+                        check=False
+                if check:
+                    right=mid
+                else:
+                    left=mid+1
+            if nums[mid]<nums[right]:
+                right=mid
+        return nums[left]
+
+
+# Solution 2
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         length=len(nums)

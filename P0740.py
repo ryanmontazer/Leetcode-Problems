923689efe152493e62deb93fc8a7b18f47083568
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-01-10 16:30:12 -0800

    Update P0740.py

--- P0740.py
+++ P0740.py
@@ -1,1 +1,13 @@
-
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def deleteAndEarn(self, nums: List[int]) -> int:
+        nums_max=max(nums)
+        points=[0]*(nums_max+1)
+        for num in nums:
+            points[num]+=num
+        DP=[0]*(nums_max+1)
+        DP[1]=points[1]
+        for i in range (2, nums_max+1):
+            DP[i]=max(DP[i-1],DP[i-2]+points[i])
+        return DP[nums_max]

4c63314a5d9c09c23befa12e40baf8b8681b5d36
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-03-11 10:18:10 -0700

    Create P0092.py

---
+++ P0092.py
@@ -0,0 +1,25 @@
+# Time: O(n)
+# Space: O(1)
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        dummy_head=ListNode(0,head)
+        old_head=dummy_head
+        for i in range(left-1):
+            old_head=old_head.next
+        reversed_tail=old_head.next #do not touch it
+        current=reversed_tail.next
+        reversed_head=reversed_tail
+        for i in range(right-left):
+            new_current=current.next
+            old_reversed_head=reversed_head
+            reversed_head=current
+            reversed_head.next=old_reversed_head
+            current=new_current
+        reversed_tail.next=current
+        old_head.next=reversed_head
+        return dummy_head.next

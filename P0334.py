4b65351e728191f1c2d1389d4ffabe72aa178d7e
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-03 16:41:13 -0700

    Update P0334.py

--- P0334.py
+++ P0334.py
@@ -1,4 +1,19 @@
+Solution 1:
+# Time:O(n)
+# Space:O(1)
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        first,second=float('inf'),float('inf')
+        for num in nums:
+            if num<=first:
+                first=num
+            elif num<=second:
+                second=num
+            elif num>second:
+                return True
+        return False
 
+Solution2:
 # Time:O(n)
 # Space:O(1)
 class Solution:

4191744900d6d3506105946cda6746ea58ea8f17
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-03 19:55:35 -0700

    Update P0338.py

--- P0338.py
+++ P0338.py
@@ -1,1 +1,18 @@
-
+Solution 1:
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans=[0]*(n+1)
+        for i in range(1,n+1):
+            ans[i]=ans[i>>1]+(i&1)
+        return ans
+      
+Solution 2:
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans=[0]*(n+1)
+        for i in range (n+1):
+            for j in range(len(bin(i))):
+                ans[i]+=(i>>j) % 2
+        return ans

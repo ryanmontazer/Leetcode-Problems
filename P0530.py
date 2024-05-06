e65a5788713554638b96ff60ea897c394dacccf5
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-05 22:30:23 -0700

    Update P0530.py

--- P0530.py
+++ P0530.py
@@ -1,3 +1,24 @@
+Solution 1:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        array,result=self.arrayBST(root),float('inf')
+        if len(array)<2:
+            return 0
+        for i in range(1,len(array)):
+            result=int(min(result,array[i]-array[i-1]))
+        return result
+    def arrayBST(self, root: Optional[TreeNode]) -> list:
+        if not root:
+            return []
+        return self.arrayBST(root.left)+[root.val]+self.arrayBST(root.right)
+        
+Solution 2:
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):

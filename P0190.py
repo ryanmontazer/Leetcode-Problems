46aa491598a7785aa4a2ecd0de273a6ae245324b
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-05 23:39:05 -0700

    Update P0190.py

--- P0190.py
+++ P0190.py
@@ -1,1 +1,10 @@
-
+# Time: O(32)
+# Space:O(32)
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        s=bin(n)[2:]
+        s="0"*(32-len(s))+s
+        array=[0]*32
+        for i in range(32):
+            array[i]=s[31-i]
+        return int("".join(array),2)  

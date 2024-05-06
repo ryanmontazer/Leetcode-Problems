cd6ae1c8ad5d3060ac863eb3c2cb687f4642c9e4
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-05 23:16:53 -0700

    Update P0070.py

--- P0070.py
+++ P0070.py
@@ -1,1 +1,11 @@
-
+# Time: O(n)
+# Space: O(n)
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n<4:
+            return n
+        result=[0]*(n+1)
+        result[1],result[2],result[3]=1,2,3
+        for i in range(4,n+1):
+            result[i]=result[i-2]+result[i-1]
+        return result[n]

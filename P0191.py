bf3ad079f2ae541eb75698f8a0e24a426a5ae83d
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-05 23:47:34 -0700

    Update P0191.py

--- P0191.py
+++ P0191.py
@@ -1,1 +1,9 @@
-
+# Time: O(32)
+# Space: O(1)
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        result=0
+        while n>0:
+            result+=n%2
+            n=n>>1
+        return result

4dd365173fb4b09b2a8acb2ac88c273f567f3bd0
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-10 00:41:05 -0700

    Update P0547.py

--- P0547.py
+++ P0547.py
@@ -1,1 +1,18 @@
-
+# Time: O(n^2)
+# Space: O(n)
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        result,remaining_cities=0,[0]*len(isConnected)
+        for i in range(len(isConnected)):
+            remaining_cities[i]=i
+        while remaining_cities:
+            result+=1
+            queue=[remaining_cities.pop()]
+            while queue:
+                element=queue.pop()
+                if element in remaining_cities:
+                    remaining_cities.remove(element)
+                for city in remaining_cities:
+                    if isConnected[element][city]==1:
+                        queue.append(city)
+        return result

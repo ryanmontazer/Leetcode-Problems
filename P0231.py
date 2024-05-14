449186a86907c6aae9bc07efad7f02725b4c3921
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-14 16:24:20 -0700

    Update P0231.py

--- P0231.py
+++ P0231.py
@@ -1,5 +1,21 @@
+Solution1:
+# Time: O(logn)
+# Space: O(logn)
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         if n>0 and bin(n)[2:].count("1")==1:
             return True
         return False
+
+Solution 2:
+# Time: O(logn)
+# Space: O(1)
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
+        if n<1:
+            return False
+        while n:
+            if n%2==1 and n//2>0:
+                return False
+            n=n//2
+        return True

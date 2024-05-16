d59acecb8550a93516ac89c57f4901cfc8c4438a
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-15 18:04:02 -0700

    Create P0459.py

---
+++ P0459.py
@@ -0,0 +1,9 @@
+# Time: O(n^2)
+# Space: O(n)
+class Solution:
+    def repeatedSubstringPattern(self, s: str) -> bool:
+        n=len(s)
+        for i in range(1,n//2+1):
+            if n % i ==0 and s[:i]*(n//i)==s:
+                return True
+        return False

6a8e299429d97ff41ea65f6a4b8d8754560399b8
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-22 15:45:53 -0700

    Update P0509.py

--- P0509.py
+++ P0509.py
@@ -1,1 +1,10 @@
-
+# Time: O(n)
+# Space: O(1)
+class Solution:
+    def fib(self, n: int) -> int:
+        if n<3:
+            return [0,1,1][n]
+        a,b,c=0,1,1
+        for i in range(n-2):
+            a,b,c=b,c,b+c
+        return c  

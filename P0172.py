d92f0a363ca3e0e2834ae4bbd9e5f3ce4dd03319
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-26 21:52:46 -0700

    Update P0172.py

--- P0172.py
+++ P0172.py
@@ -2,8 +2,8 @@
 # Space: O(1)
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        result,a=0,5
-        while n>=a:
-            result+=n//a
-            a*=5
+        result=0
+        while n>=1:
+            result+=n//5
+            n=n//5
         return result

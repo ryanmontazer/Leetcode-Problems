1d5e59cdfffe8b073c708f2124e016bf0067f876
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-05-29 21:49:43 -0700

    Update P0931.py

--- P0931.py
+++ P0931.py
@@ -1,1 +1,16 @@
-
+# Time: O(n^2)
+# Space: O(n)
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        raw=copy.copy(matrix[0])
+        temp=[0 for _ in range(len(matrix[0]))]
+        for i in range(1,len(matrix)):
+            for j in range(len(matrix[0])):
+                min_element=raw[j]
+                if 0<j:
+                    min_element=min(min_element,raw[j-1])
+                if j+1<len(matrix[0]):
+                    min_element=min(min_element,raw[j+1])
+                temp[j]=min_element+matrix[i][j]
+            raw=copy.copy(temp)
+        return min(raw)

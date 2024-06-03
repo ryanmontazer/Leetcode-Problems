f2261e5519f7347a7a74688e6337c038bda3c532
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-06-02 18:38:30 -0700

    Update P0300.py

--- P0300.py
+++ P0300.py
@@ -1,1 +1,12 @@
-
+# Time: O(nlog(n))
+# Space: O(n)
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        lis=[]
+        for  num in nums:
+            pos=bisect.bisect_left(lis,num)
+            if pos<len(lis):
+                lis[pos]=num
+            else:
+                lis.append(num)
+        return len(lis)

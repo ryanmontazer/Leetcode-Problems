1851a6fb015594e16968af2a8b983da1e54d6e3a
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-06-05 18:56:05 -0700

    Update P0050.py

--- P0050.py
+++ P0050.py
@@ -1,6 +1,20 @@
 # Time: O(log(n))
+# Space: O(1)
+class Solution1:
+    def myPow(self, x: float, n: int) -> float:
+        if n<0:
+            x,n=1/x,-n
+        result,power=1,x
+        while n:
+            if n%2:
+                result*=power
+            power*=power
+            n=n//2
+        return result
+
+# Time: O(log(n))
 # Space: O(log(n))
-class Solution:
+class Solution2:
     def myPow(self, x: float, n: int) -> float:
         if n<0:
             x,n=1/x,-n

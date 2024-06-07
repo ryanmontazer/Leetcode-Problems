a3a9715158527dfbfd2d8cca3b3ed930d481d95b
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-06-07 11:36:15 -0700

    Update P0007.py

--- P0007.py
+++ P0007.py
@@ -1,1 +1,18 @@
-
+# Time: O(log(n))
+# Space: O(log(n))
+class Solution:
+    def reverse(self, x: int) -> int:
+        reverse,abs_x="",abs(x)
+        while abs_x:
+            reverse+=str(abs_x%10)
+            abs_x=abs_x//10
+        result,power,index=0,1,1
+        maximum=2**31-1 if x>0 else 2**31
+        while index<=len(reverse):
+            if result<=-power*int(reverse[-index])+maximum:
+                result+=power*int(reverse[-index])
+            else:
+                return 0
+            index+=1
+            power*=10
+        return result if x>0 else -result

f53e20d85da0b6902c2eb682cc386f117caba0ae
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-06-07 19:34:07 -0700

    Update P0252.py

--- P0252.py
+++ P0252.py
@@ -1,1 +1,16 @@
+TBU
+# Time:  O(nlogn)
+# Space: O(n)
 
+class Solution(object):
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: bool
+        """
+        intervals.sort(key=lambda x: x[0])
+
+        for i in xrange(1, len(intervals)):
+            if intervals[i][0] < intervals[i-1][1]:
+                return False
+        return True

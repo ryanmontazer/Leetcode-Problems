2d385af7d517a989a39abeb5148d8d85e65ab811
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2024-06-07 21:28:22 -0700

    Update P0702.py

--- P0702.py
+++ P0702.py
@@ -1,31 +1,22 @@
-# """
-# This is ArrayReader's API interface.
-# You should not implement it, or speculate about its implementation
-# """
-#class ArrayReader:
-#    def get(self, index: int) -> int:
+TBU
+# Time:  O(logn)
+# Space: O(1)
 
-class Solution:
-    def search(self, reader: 'ArrayReader', target: int) -> int:
-        i=1
-        while reader.get(i) != 2**31-1:
-            i=2*i
-        #For Length of Array we have i//2 =< length < i
-        left,right= i//2,i
-        while left+1<right:
-            mid=(left+right)//2
-            if reader.get(mid) != 2**31-1:
-                left=mid
-            if reader.get(mid)==2**31-1:
-                right=mid
-        length=left+1
-        left,right=0,length-1
-        while left<=right:
-            mid=(left+right)//2
-            if reader.get(mid)==target:
-                return mid
-            if reader.get(mid)>target:
-                right=mid-1
+class Solution(object):
+    def search(self, reader, target):
+        """
+        :type reader: ArrayReader
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, 19999
+        while left <= right:
+            mid = left + (right-left)//2
+            response = reader.get(mid)
+            if response > target:
+                right = mid-1
+            elif response < target:
+                left = mid+1
             else:
-                left=mid+1
+                return mid
         return -1

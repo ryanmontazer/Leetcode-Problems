5cd72af20c1651e92cb52582daea526d83791653
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2025-01-31 19:31:41 +0330

    Update P0853.py

--- P0853.py
+++ P0853.py
@@ -1,3 +1,18 @@
+Solution1:
+# Time: O(nlog(n))
+# Space: O(n)
+class Solution:
+    def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
+        arrival=sorted(zip(position,speed))
+        stack=[]
+        while arrival:
+            pos,speed=arrival.pop()
+            arrival_time=(target-pos)/speed
+            if not stack or arrival_time>stack[-1]:
+                stack.append(arrival_time)
+        return len(stack)
+
+Solution2:
 # Time: O(nlog(n))
 # Space: O(n)
 class Solution:

750a02013e5248946961fbbc7b44c8d0f9647c55
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2025-02-12 17:50:29 +0330

    Update P0033.py

--- P0033.py
+++ P0033.py
@@ -1,4 +1,29 @@
-# Solution 1:
+# Solution 1
+# Time: O(log(n))
+# Space: O(1)
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left,right=0,len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+            if nums[mid]>nums[right]:
+                left=mid+1
+            else:
+                right=mid
+        index=left
+        left,right=index,index+len(nums)-1
+        while left<right:
+            mid=(left+right)//2
+            if target<=nums[mid % len(nums)]:
+                right=mid
+            else:
+                left=mid+1
+        if nums[left % len(nums)]==target:
+            return left % len(nums)
+        return -1
+
+
+# Solution 2:
 # Time: O(log(n))
 # Space: O(1)
 class Solution:
@@ -18,7 +43,7 @@ class Solution:
         return -1
 
 
-# Solutions 2:
+# Solutions 3:
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
         length=len(nums)

aa555c95fc2ba2255a3d5321c11f403c9abe1c11
Ryan Montazer <35645020+ryanmontazer@users.noreply.github.com>
2025-02-26 20:04:10 +0330

    Update P0104.py

--- P0104.py
+++ P0104.py
@@ -1,3 +1,6 @@
+# Solution1:
+# Time: O(n)
+# Space: O(h)
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,13 +9,50 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        if not root.left and not root.right:
-            return 1
-        left_depth,right_depth=0,0
-        if root.left:
-            left_depth=self.maxDepth(root.left)
-        if root.right:
-            right_depth=self.maxDepth(root.right)
-        return 1+max(left_depth,right_depth)
+        if not root: return 0
+        return 1+ max(self.maxDepth(root.left),self.maxDepth(root.right))
+
+# Solution2:
+# Time: O(n)
+# Space: O(w)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        result=0
+        stack=deque([root])
+        while stack:
+            for i in range(len(stack)):
+                element=stack.popleft()
+                if element.left:
+                    stack.append(element.left)
+                if element.right:
+                    stack.append(element.right)
+            result+=1
+        return result
+
+# Solution 3:
+# Time: O(n)
+# Space: O(w)
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root: return 0
+        stack=[[root,1]]
+        result=1
+        while stack:
+            element,level=stack.pop(0)
+            if element.left: stack.append([element.left,level+1])
+            if element.right: stack.append([element.right,level+1])
+            result=max(result,level)
+        return result
